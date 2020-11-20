7936b19ec4a314af770e88b771b0efdd9a558555
cb-rohit.r <rohit.r@chargebee.com>
2020-11-21 02:05:22 +0530

    refactored

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -12,6 +20,29 @@ func main() {
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"data": "Hello World"})
 	})
-	config.connectDB()
 	router.Run(":8000")
+}
+
+func init() {
+	mongoURI := getEnvVariable("MONGO_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!!!")
+	defer client.Disconnect(ctx)
+}
+
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return os.Getenv(key)
 }
